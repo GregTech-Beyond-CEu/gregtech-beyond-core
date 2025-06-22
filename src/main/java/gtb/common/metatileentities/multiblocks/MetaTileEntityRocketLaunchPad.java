a2493ceeacf5eb911be4011b2ff92436b087d1f6
BLocky104 <farmermick13@web.de>
2025-06-22 17:19:48 +0200

    Create MetaTileEntityRocketLaunchPad.java

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityRocketLaunchPad.java
@@ -0,0 +1,87 @@
+package gtb.common.metatileentities.multiblocks;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.jetbrains.annotations.NotNull;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+
+import gtb.api.recipes.GTBRecipeMaps;
+import gtb.api.render.GTBTextures;
+import gtb.common.block.GTBMetaBlocks;
+import gtb.common.block.blocks.GTBMultiblockCasing2;
+
+public class MetaTileEntityRocketLaunchPad extends RecipeMapMultiblockController {
+
+    public MetaTileEntityRocketLaunchPad(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.HIGH_VACUUM_EVAPORATION_RECIPES);
+    }
+
+    public IBlockState getCasingState() {
+        return GTBMetaBlocks.GTB_MULTIBLOCK_CASING2
+                .getState(GTBMultiblockCasing2.CasingType.THERMAL_EVAPORATION_CASING);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle(
+                        "CCC",
+                        "CCC",
+                        "CCC",
+                        "CCC")
+                .aisle(
+                        "CCC",
+                        "CCC",
+                        "CCC",
+                        "CCC")
+                .aisle( "~CCC~",
+                        "CZZZC",
+                        "CZZZC",
+                        "CZZZC",
+                        "~CSC~")
+                .where('S', selfPredicate())
+                .where('~', any())
+                .where('C', states(getCasingState()).setMinGlobalLimited(5)
+                        .or(autoAbilities()))
+                .where('Z',
+                        states(GTBMetaBlocks.GTB_MULTIBLOCK_CASING2
+                                .getState(GTBMultiblockCasing2.CasingType.ROCKET_BLOCK_BASE)))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(true, true, true, true, true, true, true);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return GTBTextures.THERMAL_CASING;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.POLARIZER_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityRocketLaunchPad(metaTileEntityId);
+    }
+}
