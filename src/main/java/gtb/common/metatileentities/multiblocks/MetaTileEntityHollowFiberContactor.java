91e10414ddfb70d602f0167f0a784f18b4904bc2
BLocky104 <farmermick13@web.de>
2025-07-28 16:58:12 +0200

    Create MetaTileEntityHollowFiberContactor.java

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityHollowFiberContactor.java
@@ -0,0 +1,76 @@
+package gtb.common.metatileentities.multiblocks;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockBoilerCasing;
+import gregtech.common.blocks.BlockGlassCasing;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+import gtb.api.recipes.GTBRecipeMaps;
+import gtb.common.block.GTBMetaBlocks;
+import gtb.common.block.blocks.GTBMultiblockCasing;
+import gtb.common.block.blocks.GTBMultiblockCasing2;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
+
+public class MetaTileEntityHollowFiberContactor extends RecipeMapMultiblockController {
+
+    public MetaTileEntityHollowFiberContactor(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.HOLLOW_FIBER_CONTACTOR_RECIPES);
+    }
+
+    public IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("~~~~~~~~~~~~~", "~~CCCCCCCCC~~", "P~CCCCCCCCC~~", "~~CCCCCCCCC~~", "~~~~~~~~~~~~~")
+                .aisle("~~CCGGGGGCC~~", "~~C~~~~~~~C~~", "PPCPPPPPPPCPP", "~~C~~~~~~~C~~", "~~CCGGGGGCC~~")
+                .aisle("~~CCGGGGGCC~~", "~~CPPPPPPPC~~", "~~C~~~~~~~C~~", "~~CPPPPPPPC~~", "~~SCGGGGGCC~~")
+                .aisle("~~CCGGGGGCC~~", "~~C~~~~~~~C~~", "PPCPPPPPPPCPP", "~~C~~~~~~~C~~", "~~CCGGGGGCC~~")
+                .aisle("~~~~~~~~~~~~~", "~~CGGGGGGGC~~", "~~CGGGGGGGC~P", "~~CGGGGGGGC~~", "~~~~~~~~~~~~~")
+                .where('S', selfPredicate())
+                .where('C', states(getCasingState()).setMinGlobalLimited(30)
+                        .or(autoAbilities()))
+                .where('P', states(MetaBlocks.BOILER_CASING.getState(BlockBoilerCasing.BoilerCasingType.STEEL_PIPE)))
+                .where('G', states(MetaBlocks.TRANSPARENT_CASING.getState(BlockGlassCasing.CasingType.LAMINATED_GLASS)))
+                .where('~', any())
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(true, true, true, true, true, true, true);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.SOLID_STEEL_CASING;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.ASSEMBLER_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityHollowFiberContactor(metaTileEntityId);
+    }
+}
