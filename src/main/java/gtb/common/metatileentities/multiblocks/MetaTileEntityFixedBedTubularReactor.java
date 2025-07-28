fa6132ffdbbb8bc215ab23bf28c353a061da1a9c
BLocky104 <farmermick13@web.de>
2025-07-28 16:58:08 +0200

    Create MetaTileEntityFixedBedTubularReactor.java

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityFixedBedTubularReactor.java
@@ -0,0 +1,72 @@
+package gtb.common.metatileentities.multiblocks;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
+import gregtech.common.blocks.BlockBoilerCasing;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+import gtb.api.recipes.GTBRecipeMaps;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+
+public class MetaTileEntityFixedBedTubularReactor extends RecipeMapMultiblockController {
+
+    public MetaTileEntityFixedBedTubularReactor(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.FIXED_BED_TUBULAR_REACTOR_RECIPES);
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity metaTileEntityHolder) {
+        return new MetaTileEntityFixedBedTubularReactor(metaTileEntityId);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("~~~~~", "~CCC~", "~CCC~", "~CCC~", "~~~~~")
+                .aisle("~CCC~", "C~~~C", "C~~~C", "C~~~C", "~CSC~")
+                .aisle("~PPP~", "P~~~P", "P~~~P", "P~~~P", "~PPP~")
+                .aisle("~PPP~", "P~~~P", "P~~~P", "P~~~P", "~PPP~")
+                .aisle("~PPP~", "P~~~P", "P~~~P", "P~~~P", "~PPP~")
+                .aisle("~CCC~", "C~~~C", "C~~~C", "C~~~C", "~CCC~")
+                .aisle("~~~~~", "~CCC~", "~CCC~", "~CCC~", "~~~~~")
+                .where('~', any())
+                .where('S', selfPredicate())
+                .where('C', states(getCasingState()).setMinGlobalLimited(5).or(autoAbilities()))
+                .where('P', states(MetaBlocks.BOILER_CASING.getState(BlockBoilerCasing.BoilerCasingType.STEEL_PIPE)))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(true, true, true, true, true, true, false);
+    }
+
+    private static IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID);
+    }
+
+    @Nonnull
+    @Override
+    protected OrientedOverlayRenderer getFrontOverlay() {
+        return Textures.ASSEMBLER_OVERLAY;
+    }
+
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart iMultiblockPart) {
+        return Textures.SOLID_STEEL_CASING;
+    }
+}
