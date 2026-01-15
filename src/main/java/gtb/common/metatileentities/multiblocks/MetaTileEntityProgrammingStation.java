0a2d00121107281f10c64e2a8d1ddc77b575cfab
BLocky104 <farmermick13@web.de>
2026-01-15 15:11:46 +0100

    Create MetaTileEntityProgrammingStation.java

---
+++ src/main/java/gtb/common/metatileentities/multiblocks/MetaTileEntityProgrammingStation.java
@@ -0,0 +1,74 @@
+package gtb.common.metatileentities.multiblocks;
+
+import gregicality.multiblocks.api.render.GCYMTextures;
+import gregicality.multiblocks.common.block.GCYMMetaBlocks;
+import gregicality.multiblocks.common.block.blocks.BlockLargeMultiblockCasing;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.util.RelativeDirection;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+import gtb.api.recipes.GTBRecipeMaps;
+import gtb.common.block.GTBMetaBlocks;
+import gtb.common.block.blocks.GTBMultiblockCasing3;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
+
+public class MetaTileEntityProgrammingStation extends RecipeMapMultiblockController {
+
+    public MetaTileEntityProgrammingStation(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTBRecipeMaps.PROGRAMMING_STATION);
+    }
+
+    public IBlockState getCasingState() {
+        return GCYMMetaBlocks.LARGE_MULTIBLOCK_CASING.getState(BlockLargeMultiblockCasing.CasingType.NONCONDUCTING_CASING);
+    }
+
+    @Override
+    protected @NotNull BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(RelativeDirection.RIGHT, RelativeDirection.BACK, RelativeDirection.UP)
+                .aisle("CCC", "CCC", "CCC")
+                .aisle("CCC", "CXC", "CCC")
+                .aisle("CCC", "CXC", "CCC")
+                .aisle("CCC", "CXC", "CCC")
+                .aisle("CCC", "CCC", "CSC")
+                .where('S', selfPredicate())
+                .where('~', any())
+                .where('C', states(getCasingState()).setMinGlobalLimited(26).or(autoAbilities()))
+                .where('X', states(GTBMetaBlocks.GTB_MULTIBLOCK_CASING3.getState(GTBMultiblockCasing3.CasingType.COMPUTER_CORE_CASING)))
+                .build();
+    }
+
+    @Override
+    public TraceabilityPredicate autoAbilities() {
+        return autoAbilities(true, true, true, true, true, true, false);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return GCYMTextures.NONCONDUCTING_CASING;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.ASSEMBLER_OVERLAY;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityProgrammingStation(metaTileEntityId);
+    }
+}
