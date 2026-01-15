4e78f9729ee5348802c12e374ee64c2f178e515d
BLocky104 <farmermick13@web.de>
2026-01-15 15:11:55 +0100

    Update KevTestRecipes.java

--- src/main/java/gtb/loaders/recipe/KevTestRecipes.java
+++ src/main/java/gtb/loaders/recipe/KevTestRecipes.java
@@ -4,16 +4,17 @@ import gregtech.api.unification.material.Materials;
 import gregtech.api.unification.ore.OrePrefix;
 
 import gtb.api.recipes.GTBRecipeMaps;
+import gtb.api.unification.materials.GTBMaterials;
 
 public class KevTestRecipes {
 
     public static void register() {
         GTBRecipeMaps.PARTICLE_ACCELERATOR_RECIPES.recipeBuilder()
-                .input(OrePrefix.ingot, Materials.Steel)
-                .output(OrePrefix.plate, Materials.Steel)
-                .kEv(270)
-                .EUt(100)
-                .duration(100)
+                .input(OrePrefix.dust, GTBMaterials.Iron54)
+                .output(OrePrefix.dust, GTBMaterials.Manganese54)
+                .kEv(800)
+                .EUt(32000)
+                .duration(800)
                 .buildAndRegister();
     }
 }
